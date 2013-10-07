73d0f7b205e9421e81c92bfb59e8e9f7352f316b
Robert Burkhead <rmburkhead@gmail.com>
2013-10-06 22:24:31 -0700

    Add license information and GPL modification notice.

--- keep-empty-dirs.py
+++ keep-empty-dirs.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+
+# This program is licensed under the GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html)
+# See the accompanying LICENSE file.
+#
+# GPL Modification Notice:
+# This program is modified, beginning in October 2013, from the original. See the accompanying README.md file for information on,
+# and access to the original author and source.
+
 """
 Recurse through the given directories, find the empty subdirectories and add a
 .keep file in each. This is used to mark all existing empty subdirectories in a
